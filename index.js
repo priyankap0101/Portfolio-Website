4257a93dce867c451ae262d96d9ca958c4c6f859
PriyankaP0101 <pandharbalepr1997@gmail.com>
2023-05-27 18:28:37 +0530

    Added all files

---
+++ index.js
@@ -0,0 +1,10 @@
+// alert("welcome to my PortFolio");
+
+ 
+function download() {
+    if (confirm ('Download file?')) {
+        location.href = './Resume/PriyankaResume.pdf';
+    }
+    
+    return false;
+    }
